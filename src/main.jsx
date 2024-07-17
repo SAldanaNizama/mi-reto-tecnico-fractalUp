27dc4af5ad7ae2887669482d01bbb78b5787138b
SAldanaNizama <140188751+SAldanaNizama@users.noreply.github.com>
2024-07-17 13:25:51 -0500

    main-app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { ApolloProvider } from '@apollo/client';
-import client from './graphql/client';
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ApolloProvider } from "@apollo/client";
+import client from "./graphql/client";
+import React from "react";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
